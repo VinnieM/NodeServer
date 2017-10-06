6b2160604e84379c0b05dff7b2b179ef54d0d180
Vineeth <vineeth.menon1@wipro.com>
2017-10-06 13:09:52 +0530

    Added the function getAPIFromCatalog and checkObjectState

--- models/GetRequestProcessor.js
+++ models/GetRequestProcessor.js
@@ -12,40 +12,74 @@ var fileReader = require('file-system');
 router.use(jsonParser.urlencoded({extended: true}));
 router.use(jsonParser.json());
 
-var apiCatalog = loadAPICatalog(function(data){
-  apiCatalog = data;
-});
+// var apiCatalog = loadAPICatalog(function(data){
+//   apiCatalog = data;
+// });
 
 router.route('/:paramName')
-  .get(function (request, response) {
-    var apiName = request.param('paramName');
-
+.get(function (request, response) {
+    var paramName = request.param('paramName');
     // Checking for the length of the API
-    if(apiName.length<=1){
+    if(paramName.length<1){
       response.json({status: false, message: 'Please enter a valid API'});
     }
-    // Happy router
+    // Check if API matches from the catalog
+    var apiPath = getAPIFromCatalog(paramName, function(data){
+      apiPath = data;
+    });
+    //Checks if the response is an error object or a valid response
+    var isApiValid = checkObjectState(apiPath);
+    if(!(isApiValid)){
+      response.send(apiPath);
+    }
     response.json({
       status:true,
-      message: 'Happy Route'
+      message: 'The API path is '+apiPath
     });
 });
 
-function getAPIFromCatalog(error, path, api, callBack){
-  var returnObject;
-  properties = PropertiesReader(path);
-  if(error){
-    console.log('Error Reading property file from '+path);
-    returnObject = JSON.stringify({
-      status: false,
-      message: error
-    });
-    return callBack(returnObject);
+
+/**
+ * checkObjectState - This method checks if the passed parameter is a JSON Object or not.
+ *
+ * @param  {var}  val Can be any variable
+ * @return {boolean}  If the parameter is a JSON Object false is returned, else true.
+ */
+function checkObjectState(val){
+  try{
+    JSON.parse(val);
+  } catch(exception){
+    return true;
   }
-  returnObject = properties.get(api);
-  return callBack(returnObject);
+  return false;
 }
 
+
+/**
+ * getAPIFromCatalog - This function checks the APICatalog.js for the appropriate API link.
+ *
+ * @param  {var}  api The API which needs to be checked.
+ * @param  {type} callback
+ * @return {JSON} {String} This method will return a JSON Object in case of an error. If the
+ *                          API is found the path to API is returned as a String.
+ */
+function getAPIFromCatalog(api, callback){
+  var fetchApi = apiCatalog[api];
+  if(fetchApi===undefined){
+    var errorObject = JSON.stringify({
+      status: false,
+      message: 'Unable to find API '+api
+    });
+    return errorObject;
+  }
+  return fetchApi;
+}
+
+
+/**
+ * loadAPICatalog - Depricated function
+ *
+ */
 function loadAPICatalog(callback){
   var returnData;
   fileReader.readFile('../NodeServer/lib/APICatalog.properties', function (error, data){
