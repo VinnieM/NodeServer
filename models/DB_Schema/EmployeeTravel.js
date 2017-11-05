bfdca734faae98b423ca8791ee1cc5452a2a227f
Vineeth <vineeth.madhu@gmail.com>
2017-11-06 03:38:00 +0530

    Added the DB and the collection to the Schema

--- models/DB_Schema/EmployeeTravel.js
+++ models/DB_Schema/EmployeeTravel.js
@@ -4,11 +4,14 @@
 var mongoConnection = require('mongoose');
 var Schema = mongoConnection.Schema;
 // Connecting to MongoDB
-mongoConnection.connect('mongodb://127.0.0.1:27017/');
+mongoConnection.connect('mongodb://127.0.0.1:27017/BOSTravel');
+var mongoDb =   mongoConnection.connection;
+//Bind connection to error event (to get notification of connection errors)
+mongoDb.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var employeeSchema = new Schema({
   email: String,
   employeeId: String
 });
 
-module.exports = mongoConnection.model('EmployeeDetails', employeeSchema);
+module.exports = mongoConnection.model('EmployeeDetails', employeeSchema, 'user-context-collection');
