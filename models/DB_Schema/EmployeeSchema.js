21bee040a2a6f754f1134e420ba66a8b44ba8e24
Vineeth <vineeth.madhu@gmail.com>
2017-11-06 04:05:39 +0530

    Adding UserContext EmployeeSchema

---
+++ models/DB_Schema/EmployeeSchema.js
@@ -0,0 +1,21 @@
+'use strict'
+//Custom imports
+var constants = require('../../lib/constants.js');
+
+//Node_module imports
+var mongoConnection = require('mongoose');
+var Schema = mongoConnection.Schema;
+
+// Connecting to MongoDB
+mongoConnection.connect(constants.MongoDBConnection);
+var mongoDb =   mongoConnection.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+mongoDb.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var employeeSchema = new Schema({
+  email: String,
+  employeeId: String
+});
+
+module.exports = mongoConnection.model('EmployeeDetails', employeeSchema, 'user-context-collection');
