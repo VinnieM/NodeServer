60d8e994a4c4a9828fe8a84904fcc5568daa03e5
Vineeth <vineeth.madhu@gmail.com>
2017-11-06 15:20:03 +0530

    Added an API which retrives user information from MongoDB

--- models/API/Employee.js
+++ models/API/Employee.js
@@ -1,6 +1,7 @@
 'use strict';
 //Custom imports
 var constants = require('../../lib/constants.js');
+var userContext = require('../DB_Schema/EmployeeSchema.js');
 
 //Node_module imports
 var express = require('express');
@@ -42,14 +43,31 @@ router.route('/' + constants.APIVersion + '/getEmployeeDetails')
   });
 
 /**
- * This is a test API which takes a parameter
+ * This is an API which return the basic details of an employee
+ *
+ * @param empNumber The employee number needs to be passed a GET parameter
  */
-router.route('/' + constants.APIVersion + '/getEmployeeDetails' + '/:paramValue')
+router.route('/' + constants.APIVersion + '/getEmployeeDetails' + '/:empNumber')
   .get(function (request, response) {
-    var param = request.param('paramValue');
-    response.json({
-      status: true,
-      paramValue: param
+    var employeeNumber = request.param('empNumber');
+    userContext.find({
+      'empID': employeeNumber
+    }, function (err, data) {
+      if (err) {
+        console.log('The error is ' + err);
+      } else {
+        var empDetails = JSON.parse(JSON.stringify(data[0]));
+        response.json({
+          status: true,
+          Employee_Name: empDetails.empDetails.empName,
+          Employee_ADID: empDetails.empDetails.empADID,
+          Employee_Band: empDetails.empDetails.empBand,
+          Employee_Passport_Number: empDetails.passportDetails.passportNo,
+          Employee_Manager_Name: empDetails.supDetails.Name,
+          Employee_Manager_ADID: empDetails.supDetails.ADID,
+          Employee_Manager_Email: empDetails.supDetails.Email
+        });
+      }
     });
   });
 
