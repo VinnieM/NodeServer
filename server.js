ec9010b2317651c6a186aa0424a51f52685676b1
Vineeth <vineeth.madhu@gmail.com>
2017-11-16 17:06:57 +0000

    Updated the server.js with the passport Basic auth

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 var getRequests = require('./models/GetRequestProcessor.js');
 var postRequests = require('./models/PostRequestProcessor.js');
 var constants = require('./lib/constants.js');
+var db = require('./models/DB_Schema/users.js');
 
 // Node_Module imports
 var express = require('express');
 var server = express();
 var jsonParser = require('body-parser');
+var passport = require('passport');
+var Strategy = require('passport-http').BasicStrategy;
 
 // API's
 var employee = require('./models/API/Employee.js');
@@ -26,20 +29,42 @@ var router = express.Router([{
   caseSensitive: true
 }]);
 
+passport.use(new Strategy(
+  function (username, password, cb) {
+    db.findByUsername(username, function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false);
+      }
+      if (user.password != password) {
+        return cb(null, false);
+      }
+      return cb(null, user);
+    });
+  }));
+
 // Using the router for processing GET requests
 router.use('/' + constants.APIVersion + '/invokeGet', getRequests);
 // Using the router for processing POST requests
 router.use('/' + constants.APIVersion + '/invokePost', postRequests);
 
 // Welcome GET request
-router.get('/', function (request, response) {
-  response.json({
-    message: 'Welcome to Node server v0.1'
+
+router.get('/', passport.authenticate('basic', {
+    session: false
+  }),
+  function (request, response) {
+    response.json({
+      message: 'Welcome to Node server v0.1'
+    });
   });
-});
 
 // Welcome POST request
-router.post('/', function (request, response) {
+router.post('/', passport.authenticate('basic', {
+  session: false
+}), function (request, response) {
   response.json({
     message: 'Welcome to Node server v0.1'
   });
